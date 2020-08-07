bb77f039d31e5538a2e8b29cb45e8dafbd194910
Rafaela Pitta <rafaelapitta@MacBook-Pro.local>
2020-08-07 13:41:06 -0400

    css-update

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -16,9 +16,8 @@ function Landing() {
       <div id="page-landing-content" className="container">
         <div className="logo-container">
           <img src={logoImg} alt="Proffy"/>
-          <h2>Sua plataforma de estudos online</h2>
-        </div>   
-
+          <h2>Sua plataforma de estudos online.</h2>
+        </div>
 
         <img 
           src={landingImg} 
@@ -26,24 +25,22 @@ function Landing() {
           className="hero-image"
         />
 
-      <div className="buttons-container">
+        <div className="buttons-container">
+          <Link to="/study" className="study">
+            <img src={studyIcon} alt="Estudar"/>
+            Estudar
+          </Link>
 
-        <Link to="/study" className="study">
-        <img src={studyIcon} alt="Estudar"/>
-        Estudar
-        </Link>
+          <Link to="give-classes" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas"/>
+            Dar aulas
+          </Link>
+        </div>
 
-        <Link to="/give-classes" className="give-classes">
-        <img src={giveClassesIcon} alt="Estudar"/>
-        Estudar
-        </Link>
-      </div>
-
-
-      <span className="total-connections">
-        Total de 200 conexões já realizadas
-        <img src={purpleHeartIcon} alt="Heart"/>
-      </span>
+        <span className="total-connections">
+          Total de 200 conexões já realizadas
+          <img src={purpleHeartIcon} alt="Coração roxo"/>
+        </span>
       </div>
     </div>
   )
