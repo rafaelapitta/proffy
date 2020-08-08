de8e4c377ac395f42962e702b035f7dc0e0f4dfc
Rafaela Pitta <rafaelapitta@MacBook-Pro.local>
2020-08-07 23:01:29 -0400

    update

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function pageHeader() {
-  
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+interface PageHeaderProps {
+  title: string;
+}
+
+const pageHeader: React.FunctionComponent<PageHeaderProps> = (props) => {
   return (
-  <h1>Header</h1>
+    <header className="page-header">
+    <div className="top-bar-container">
+      <Link to="/">
+        <img src={backIcon} alt="Voltar"/>
+      </Link>
+      <img src={logoImg} alt="Proffy"/>
+    </div>
 
+    <div className="header-content">
+      <strong>{props.title}</strong>
+     
+     {props.children}
+    </div>
+  </header>
   ) 
 }
 
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import PageHeader from '../../components/PageHeader';
 
-import logoImg from '../../assets/images/logo.svg';
-import backIcon from '../../assets/images/icons/back.svg';
 
 import './styles.css';
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-        <header className="page-header">
-          <div className="top-bar-container">
-            <Link to="/">
-              <img src={backIcon} alt="Voltar"/>
-            </Link>
-            <img src={logoImg} alt="Proffy"/>
+        <PageHeader title="Esses são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input id="subject" type="text"/>
           </div>
 
-          <div className="header-content">
-            <strong>Esses são os proffys disponíveis</strong>
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da semana</label>
+            <input id="week_day" type="text"/>
           </div>
-        </header>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input id="time" type="text"/>
+          </div>
+        </form>
+        </PageHeader>
     </div>
   )
 }
